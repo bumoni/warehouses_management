64489714712f313db7d23e4ff1c4733d014da5c6
Bubul Rajkhowa <rajkhowab9@gmail.com>
2023-09-24 21:01:42 +0530

    debounce

--- warehouse_management/src/store.js
+++
@@ -1,9 +0,0 @@
-// src/store.js
-import { configureStore } from '@reduxjs/toolkit';
-import warehousesReducer from './Features/warehousesSlice';
-
-export const store = configureStore({
-  reducer: {
-    warehouses: warehousesReducer,
-  },
-});
